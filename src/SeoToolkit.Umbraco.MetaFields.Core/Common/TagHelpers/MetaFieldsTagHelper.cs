ca7bc2efe76bd4fa329eb1ca563522567c4e63f0
patrickdemooij9 <patrickdemooij98@hotmail.com>
2024-12-10 20:03:23 +0100

    Fix for pages with IVirtualPageController

--- src/SeoToolkit.Umbraco.MetaFields.Core/Common/TagHelpers/MetaFieldsTagHelper.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Common/TagHelpers/MetaFieldsTagHelper.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Umbraco.Cms.Core.Web;
 using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Umbraco.Cms.Web.Common.Routing;
 
 namespace SeoToolkit.Umbraco.MetaFields.Core.Common.TagHelpers
 {
     public class MetaFieldsTagHelper : TagHelper
     {
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
         private readonly IMetaFieldsService _seoService;
         private readonly IUmbracoContextFactory _umbracoContextFactory;
 
@@ -23,7 +29,13 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Common.TagHelpers
             output.TagName = null;
 
             var stringBuilder = new StringBuilder();
-            var metaTags = _seoService.Get(ctx.UmbracoContext.PublishedRequest.PublishedContent, true);
+            var content = ctx.UmbracoContext.PublishedRequest.PublishedContent;
+            if (content is null)
+            {
+                // Fix for https://github.com/umbraco/Umbraco-CMS/issues/12834
+                content = ViewContext.HttpContext.Features.Get<UmbracoRouteValues>().PublishedRequest.PublishedContent;
+            }
+            var metaTags = _seoService.Get(content, true);
             if (metaTags is null)
                 return;
             foreach (var (key, value) in metaTags.Fields)
