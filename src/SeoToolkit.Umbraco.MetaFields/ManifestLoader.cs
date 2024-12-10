8be89dad71aa8f9fb9fc0d2ab8ca24ff452e07c6
patrickdemooij9 <patrickdemooij98@hotmail.com>
2024-12-10 20:05:53 +0100

    Up versions

--- src/SeoToolkit.Umbraco.MetaFields/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.MetaFields/ManifestLoader.cs
@@ -20,7 +20,7 @@ namespace SeoToolkit.Umbraco.MetaFields
             manifests.Add(new PackageManifest
             {
                 PackageName = "SeoToolkit.Umbraco.MetaFields",
-                Version = "3.6.0",
+                Version = "3.6.1",
                 Scripts = new[]
                 {
                     "/App_Plugins/SeoToolkit/MetaFields/Interface/ContentApps/DocumentSettings/documentSettings.controller.js",
